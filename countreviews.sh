a6832b178fa57d98cce69ada3f6025bddafc377b
db5n17 <db5n17@soton.ac.uk>
2019-02-28 03:29:28 +0000

    Modified the countreviews.sh file to be more efficient.

--- countreviews.sh
+++ countreviews.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-ls $1 | while read file; do 
-	cat $1/$file | while read line; do 
-		grep -E "<Author>";
-	done | echo "${file%.*} $(wc -l)"
+for file in $1/*; do 
+	echo "${file%.*} $(grep -E -c "<Author>" $file)"
 done | sort -n -r -k 2
