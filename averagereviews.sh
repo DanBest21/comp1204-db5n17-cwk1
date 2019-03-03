26a9dab06270a99ea8b59d484ba6991af4a6f165
db5n17 <db5n17@soton.ac.uk>
2019-03-03 04:47:59 +0000

    Implemented the intended functionality for the averagereviews.sh script.

--- averagereviews.sh
+++ averagereviews.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 for file in $1/*; do
-	echo "$(basename $file .dat) $(grep -E "<Overall>" $file)"
+	total=0
+	for score in $(grep "<Overall>" $file | sed 's/^.*<Overall>//; s/\r$//'); do
+		total=$((total + score));
+	done
+	echo "$(basename $file .dat) $(echo $total $(grep -E -c "<Author>" $file) | awk '{printf("%.2f\n", $1/$2)}')";
 done | sort -n -r -k 2
