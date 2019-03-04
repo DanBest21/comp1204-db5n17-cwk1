d938c7712a8e311c4849db9694fd418ed9675850
db5n17 <db5n17@soton.ac.uk>
2019-03-04 03:23:49 +0000

    Added more comments to detail how each script works.

--- averagereviews.sh
+++ averagereviews.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+# For each file in the given file path:
 for file in $1/*; do
+	# Instantiate two variables, total and i, and set them both to 0 at each iteration.
 	total=0
-	for score in $(grep "<Overall>" $file | sed 's/^.*<Overall>//; s/\r$//'); do
-		total=$((total + score));
+	i=0
+	# For each rating, i.e. the number found after the <Overall> tag, which is retrieved by
+	# means of piping the output of grep <Overall> into a sed command that removes
+	# the actual <Overall> tag as well as the return character that suceeds it:
+	for rating in $(grep "<Overall>" $file | sed 's/^.*<Overall>//; s/\r$//'); do
+		# Sum the rating values to get the total.
+		total=$((total + rating))
+		# Increment the i variable up by one.
+		i=$((i + 1));
 	done
-	echo "$(basename $file .dat) $(echo $total $(grep -E -c "<Author>" $file) | awk '{printf("%.2f\n", $1/$2)}')";
+	# Output the basename of the file followed by the total variable divided by the i
+	# variable by means of piping these values into an awk command that formats them
+	# to two decimal places.
+	echo "$(basename $file .dat) $(echo $total $i | awk '{printf("%.2f\n", $1/$2)}')";
+# Once the loop is completed, feed the output via a pipeline to a sort command, which sorts
+# numerically (-n), in reverse order/highest to lowest (-r) and the field which needs to be
+# sorted (-k 2).
 done | sort -n -r -k 2
--- countreviews.sh
+++ countreviews.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-for file in $1/*; do 
+# For each file in the given file path:
+for file in $1/*; do
+        # Output the basename of the file followed by the number of <Author> tags in that 
+	# file - which will represent the number of reviews.	
 	echo "$(basename $file .dat) $(grep -E -c "<Author>" $file)"
+# Once the loop is completed, feed the output via a pipeline into the sort command, which sorts
+# numerically (-n), in reverse order/highest to lowest (-r) and the field which needs to be
+# sorted (-k 2).
 done | sort -n -r -k 2
