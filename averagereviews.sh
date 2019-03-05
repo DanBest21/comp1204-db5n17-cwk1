a9fdbffc36663a374ce9efea7d8c5d98968ccfd7
db5n17 <db5n17@soton.ac.uk>
2019-03-05 00:33:38 +0000

    Consolidated the comments in both script files.

--- averagereviews.sh
+++ averagereviews.sh
@@ -4,10 +4,15 @@
 for file in $1/*; do
 	total=0
 	i=0
+	# For each rating found (i.e. the number after each <Overall> tag in the file):
 	for rating in $(grep "<Overall>" $file | sed 's/^.*<Overall>//; s/\r$//'); do
+		# Add the rating to the total and increment the i value.
 		total=$((total + rating))
 		i=$((i + 1));
 	done
+	# Get the name of the file without the extension and file path.
 	name=$(basename $file .dat)
+	# Calculate the overall rating and round it to two decimal places using awk, and then output this.
 	echo | awk -v name="$name" -v total="$total" -v i="$i" '{printf "%s %.2f\n", name, total/i}';
+# Feed the output of the loop into a sort command.
 done | sort -n -r -k 2
--- countreviews.sh
+++ countreviews.sh
@@ -2,10 +2,7 @@
 
 # For each file in the given file path:
 for file in $1/*; do
-        # Output the basename of the file followed by the number of <Author> tags in that 
-	# file - which will represent the number of reviews.	
+        # Output the basename of the file, and the number of reviews (number of <Author> tags).	
 	echo "$(basename $file .dat) $(grep -E -c "<Author>" $file)"
-# Once the loop is completed, feed the output via a pipeline into the sort command, which sorts
-# numerically (-n), in reverse order/highest to lowest (-r) and the field which needs to be
-# sorted (-k 2).
+# Feed the output of the loop into a sort command.
 done | sort -n -r -k 2
