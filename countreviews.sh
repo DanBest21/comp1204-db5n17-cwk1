fcddb6601932176cc5d9c63d600fa9f4de9a26c8
COMP1204 Student <student@comp1204-vm>
2019-03-05 15:35:56 +0000

    Removed uneccessary "-E" argument in the grep command.

--- countreviews.sh
+++ countreviews.sh
@@ -3,6 +3,6 @@
 # For each file in the given file path:
 for file in $1/*; do
         # Output the basename of the file, and the number of reviews (number of <Author> tags).	
-	echo "$(basename $file .dat) $(grep -E -c "<Author>" $file)"
+	echo "$(basename $file .dat) $(grep -c "<Author>" $file)"
 # Feed the output of the loop into a sort command.
 done | sort -n -r -k 2
